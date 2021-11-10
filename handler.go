9acb2fa8fb379f037428e2db043c030b2e765d37
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2021-11-10 13:55:05 +0000

    inital commit

---
+++ handler.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("Hello from Azure Functions! This is the %s environment!\n", os.Getenv("ENV"))
+	fmt.Fprint(w, message)
+}
+
+func main() {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	http.HandleFunc("/api/hello", helloHandler)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
